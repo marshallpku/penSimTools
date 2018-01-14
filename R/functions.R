9322892a8f0283e41d6425b765fca36c74763017
Yimeng Yin <marshallpku@gmail.com>
2018-01-13 21:18:21 -0500

    add doc

--- R/functions.R
+++ R/functions.R
@@ -110,3 +110,17 @@ create_returns <- function(r.mean, r.sd, period){
 "mtcars"
 
 
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"sampleData"
+
